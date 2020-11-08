b216e4e58263c09082280e3aabad2da8c3fb6740
Thilan Tran <thilanoftran@gmail.com>
2020-11-08 12:49:14 -0800

    Revert "add Map component"
    This reverts commit 8d1505fd579062e6c33971e094f8d7cd498e0a6d.

--- src/components/StateProvider.tsx
+++ src/components/StateProvider.tsx
@@ -3,42 +3,41 @@ import React, { useEffect, useReducer } from 'react';
 import { SetUserAction, State, StateAndDispatch } from './StateProviderTypes';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAcnkdEHyVz37TMFt7tj-_6KnOL3f7l9Bw',
-  authDomain: 'bruinbot-8d68e.firebaseapp.com',
-  databaseURL: 'https://bruinbot-8d68e.firebaseio.com',
-  projectId: 'bruinbot-8d68e',
-  storageBucket: 'bruinbot-8d68e.appspot.com',
-  messagingSenderId: '925978645479',
-  appId: '1:925978645479:web:5aa6b096d01bf1d4596342'
+	apiKey: 'AIzaSyAcnkdEHyVz37TMFt7tj-_6KnOL3f7l9Bw',
+	authDomain: 'bruinbot-8d68e.firebaseapp.com',
+	databaseURL: 'https://bruinbot-8d68e.firebaseio.com',
+	projectId: 'bruinbot-8d68e',
+	storageBucket: 'bruinbot-8d68e.appspot.com',
+	messagingSenderId: '925978645479',
+	appId: '1:925978645479:web:5aa6b096d01bf1d4596342'
 };
 firebase.initializeApp(firebaseConfig);
 
 const initialState: State = { firebase, user: null };
 const Ctx = React.createContext<StateAndDispatch>(undefined!);
 
-const StateProvider = (props: { children: React.ReactNode }) => {
-  const [state, dispatch] = useReducer(
-    (state: State, action: SetUserAction): State => {
-      // write actions here
-      if (action.type === 'SET_USER') {
-        return {
-          ...state,
-          user: action.user
-        };
-      }
-      return state;
-    },
-    initialState
-  );
+const StateProvider = (props: { children: React.ReactNode; }) => {
+	const [state, dispatch] = useReducer((state: State, action: SetUserAction): State => {
+		// write actions here
+		if (action.type === 'SET_USER') {
+			return {
+				...state,
+				user: action.user,
+			};
+		}
+		return state;
+	}, initialState);
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(function(user) {
-      dispatch({ type: 'SET_USER', user });
-    });
-  }, []);
-  return (
-    <Ctx.Provider value={{ state, dispatch }}>{props.children}</Ctx.Provider>
-  );
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(function (user) {
+			dispatch({ type: 'SET_USER', user });
+		});
+	}, []);
+	return (
+		<Ctx.Provider value={{ state, dispatch }}>
+			{props.children}
+		</Ctx.Provider>
+	);
 };
 
 export { Ctx, StateProvider };
--- src/containers/LoginScreen.tsx
+++ src/containers/LoginScreen.tsx
@@ -1,104 +1,99 @@
 import { FirebaseError } from 'firebase';
 import React, { useContext, useEffect, useState } from 'react';
 import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  View
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	View
 } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import { Ctx } from '../components/StateProvider';
 
 // TODO: don't use `any`
 const LoginScreen = (props: any) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { state } = useContext(Ctx);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const { state } = useContext(Ctx);
 
-  useEffect(() => {
-    if (state.user) props.navigation.navigate('Blank');
-  });
+	useEffect(() => {
+		if (state.user)
+			props.navigation.navigate('Blank');
+	});
 
-  return (
-    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-        style={styles.container}
-      >
-        <View style={styles.logoContainer}>
-          <Text>Logo goes here!</Text>
-        </View>
-        <View style={styles.formContainer}>
-          <Input
-            placeholder="Email"
-            containerStyle={{ marginBottom: 8 }}
-            onChangeText={(text) => setEmail(text)}
-          />
-          <Input
-            placeholder="Password"
-            containerStyle={{ marginBottom: 16 }}
-            secureTextEntry={true}
-            onChangeText={(text) => setPassword(text)}
-          />
-          <Button
-            title="Log in"
-            buttonStyle={{ width: 128 }}
-            containerStyle={{ marginBottom: 16 }}
-            onPress={() => {
-              state.firebase
-                .auth()
-                .signInWithEmailAndPassword(email, password)
-                .catch(function(_error: FirebaseError) {
-                  // TODO: Handle error codes
-                  console.log(_error.message);
-                });
-            }}
-          />
-          <Button
-            title="Sign up"
-            buttonStyle={{ width: 128 }}
-            containerStyle={{ marginBottom: 16 }}
-            onPress={() => {
-              state.firebase
-                .auth()
-                .createUserWithEmailAndPassword(email, password)
-                .catch(function(_error: FirebaseError) {
-                  // TODO: Handle error codes
-                  console.log(_error.message);
-                });
-            }}
-          />
-        </View>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
-  );
+	return (
+		<TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+			<KeyboardAvoidingView
+				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+				style={styles.container} >
+				<View style={styles.logoContainer}>
+					<Text>
+						Logo goes here!
+					</Text>
+				</View>
+				<View style={styles.formContainer}>
+					<Input
+						placeholder='Email'
+						containerStyle={{ marginBottom: 8 }}
+						onChangeText={(text) => setEmail(text)}
+					/>
+					<Input
+						placeholder='Password'
+						containerStyle={{ marginBottom: 16 }}
+						secureTextEntry={true}
+						onChangeText={(text) => setPassword(text)}
+					/>
+					<Button
+						title="Log in"
+						buttonStyle={{ width: 128 }}
+						containerStyle={{ marginBottom: 16 }}
+						onPress={() => {
+							state.firebase.auth().signInWithEmailAndPassword(email, password).catch(function (_error: FirebaseError) {
+								// TODO: Handle error codes
+								console.log(_error.message);
+							});
+						}}
+					/>
+					<Button
+						title="Sign up"
+						buttonStyle={{ width: 128 }}
+						containerStyle={{ marginBottom: 16 }}
+						onPress={() => {
+							state.firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (_error: FirebaseError) {
+								// TODO: Handle error codes
+								console.log(_error.message);
+							});
+						}}
+					/>
+				</View>
+			</KeyboardAvoidingView >
+		</TouchableWithoutFeedback>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'stretch',
-    backgroundColor: '#fff'
-  },
-  logoContainer: {
-    height: '40%',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    paddingBottom: 24
-  },
-  formContainer: {
-    height: '60%',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 24,
-    paddingHorizontal: 24,
-    backgroundColor: '#fff'
-  }
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'stretch',
+		backgroundColor: '#fff',
+	},
+	logoContainer: {
+		height: '40%',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		paddingBottom: 24
+	},
+	formContainer: {
+		height: '60%',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		paddingTop: 24,
+		paddingHorizontal: 24,
+		backgroundColor: '#fff',
+	}
 });
 
 export default LoginScreen;
-
