d68ab705df610928057ced6d7d227dd771dcd027
Thilan Tran <44995807+thilan-tran@users.noreply.github.com>
2020-11-12 16:19:22 -0800

    Add swipeable inventory component, and link it to map markers (#8)
    * Add swipeable inventory component, and link it to map markers
    
    * Add comments and remove unnecessary useEffect
    
    * Remove unused useEffect dep
    
    * Add swipeable inventory component, and link it to map markers

--- src/containers/MapScreen.tsx
+++ src/containers/MapScreen.tsx
@@ -1,16 +1,82 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import MapComponent from '../components/MapView';
-import CampusData from '../assets/campusCoords.json';
-import Tank from '../assets/tank.png';
+import MapComponent, { MapControlledTypes } from '../components/MapView';
+import Inventory from '../components/InventoryView';
 
-let botCoords = [
-	// placeholders
-	{ id: 1, name: 'Optimus', latitude: 34.0714, longitude: -118.4432 },
-	{ id: 2, name: 'Bumble', latitude: 34.0735, longitude: -118.4439 },
-	{ id: 3, name: 'Zilla', latitude: 34.0692, longitude: -118.4463 },
+import CampusData from '../assets/campusCoords.json';
+import Ham from '../assets/greenHam.jpg';
+import Tank from '../assets/tank.png';
+import Bot from '../assets/robot.png';
+import Crane from '../assets/crane.png';
+import Marker from '../assets/marker.png';
+
+// placeholder data
+const botCoords = [
+	{
+		id: 'A',
+		name: 'Optimus',
+		latitude: 34.0714,
+		longitude: -118.4432,
+	},
+	{
+		id: 'B',
+		name: 'Bumble',
+		latitude: 34.0735,
+		longitude: -118.4439,
+	},
+	{
+		id: 'C',
+		name: 'Zilla',
+		latitude: 34.0692,
+		longitude: -118.4463,
+	},
 ];
+
+const botInfo = {
+	A: {
+		name: 'Optimus',
+		distance: 0.5,
+		inventorySize: 20,
+		itemsSold: 15,
+		imgSrc: Tank,
+	},
+	B: {
+		name: 'Bumble',
+		distance: 0.7,
+		inventorySize: 15,
+		itemsSold: 2,
+		imgSrc: Crane,
+	},
+	C: {
+		name: 'Zilla',
+		distance: 0.2,
+		inventorySize: 3,
+		itemsSold: 5,
+		imgSrc: Bot,
+	},
+};
+
+const botInventories = {
+	A: new Array(10).fill(null).map((_, idx) => ({
+		id: idx,
+		name: 'Green Eggs and Ham',
+		price: 4.19,
+		imgSrc: Ham,
+	})),
+	B: new Array(2).fill(null).map((_, idx) => ({
+		id: idx,
+		name: 'Green Eggs and Ham',
+		price: 4.29,
+		imgSrc: Ham,
+	})),
+	C: new Array(3).fill(null).map((_, idx) => ({
+		id: idx,
+		name: 'Green Eggs and Ham',
+		price: 4.39,
+		imgSrc: Ham,
+	})),
+};
 
 const shuffleMarkers = (markers) => {
 	// for testing purposes, randomly move around markers
@@ -21,6 +87,12 @@ const shuffleMarkers = (markers) => {
 	});
 };
 
+// wrapper component that takes in a dynamically updated
+// `id` prop from the MapComponent
+const MapControlledComponent = ({ id }: MapControlledTypes) => (
+	<Inventory id={id} info={botInfo} items={botInventories} />
+);
+
 const MapScreen = () => {
 	const [markers, setMarkers] = useState(botCoords);
 	return (
@@ -28,12 +100,13 @@ const MapScreen = () => {
 			<MapComponent
 				initRegion={CampusData.region}
 				markers={markers}
-				markerImg={Tank}
+				markerImg={Marker}
 				polygonCoords={CampusData.polygon.map(([lat, lng]) => ({
 					latitude: lat,
 					longitude: lng,
 				}))}
 				refresh={() => setMarkers(shuffleMarkers(markers))}
+				ControlledComponent={MapControlledComponent}
 			/>
 		</View>
 	);
