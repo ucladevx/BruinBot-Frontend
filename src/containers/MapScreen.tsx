ef8663c266bbba7dbf9932333bf2ec57eb533926
Thilan Tran <thilanoftran@gmail.com>
2020-11-12 16:12:19 -0800

    Remove unused useEffect dep

--- src/containers/MapScreen.tsx
+++ src/containers/MapScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import MapComponent, { MapControlledTypes } from '../components/MapView';
