7dad8d4dbe3d968a4eac2eec85f026d956be1a15
Max Zhang <pipi-max@hotmail.com>
2020-11-18 22:20:22 -0800

    Added QR features

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import 'react-native-gesture-handler';
+import * as Linking from 'expo-linking';
 import { Ctx, StateProvider } from './src/components/StateProvider';
 import LoginScreen from './src/containers/auth/LoginScreen';
 import PasswordResetScreen from './src/containers/auth/PasswordResetScreen';
 import SignupScreen from './src/containers/auth/SignupScreen';
 import MapScreen from './src/containers/MapScreen';
+import QrScreen from './src/containers/QrScreen';
 
 export type RootStackParamList = {
 	Login: undefined;
@@ -14,14 +16,21 @@ export type RootStackParamList = {
 	PasswordReset: undefined;
 	Blank: undefined;
 	Map: undefined;
+	Qr: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
+const prefix = Linking.makeUrl('/');
+
 export default function App() {
+	const linking = {
+		prefixes: [prefix],
+	};
+
 	return (
 		<StateProvider>
-			<NavigationContainer>
+			<NavigationContainer linking={linking}>
 				<Home />
 			</NavigationContainer>
 		</StateProvider>
@@ -29,16 +38,52 @@ export default function App() {
 }
 
 const Home = () => {
-	const { state } = useContext(Ctx);
+	const { state, dispatch } = useContext(Ctx);
+
+	const updateBotFromDeepLink = useCallback(
+		(botid: any) => {
+			if (state.bot && botid === state.bot._id) {
+				return;
+			}
+			// Dispatch with placeholder bot
+			dispatch({ type: 'SET_BOT', bot: { _id: botid } });
+			// Change it to inventory view if user not logged in
+			Linking.openURL(Linking.makeUrl('Map'));
+		},
+		[dispatch, state.bot]
+	);
+
+	useEffect(() => {
+		Linking.parseInitialURLAsync().then(({ path: _path, queryParams }) => {
+			if (queryParams && queryParams.botid) {
+				updateBotFromDeepLink(queryParams.botid);
+			}
+		});
+	}, [updateBotFromDeepLink]);
+
+	Linking.addEventListener('url', ({ url }) => {
+		if (url) {
+			const { queryParams } = Linking.parse(url);
+			if (queryParams && queryParams.botid) {
+				updateBotFromDeepLink(queryParams.botid);
+			}
+		}
+	});
+
 	return (
 		<Stack.Navigator headerMode="none">
 			{state.user ? (
-				<Stack.Screen name="Map" component={MapScreen} />
+				<>
+					<Stack.Screen name="Qr" component={QrScreen} />
+					<Stack.Screen name="Map" component={MapScreen} />
+				</>
 			) : (
 				<>
+					<Stack.Screen name="Qr" component={QrScreen} />
 					<Stack.Screen name="Login" component={LoginScreen} />
 					<Stack.Screen name="Signup" component={SignupScreen} />
 					<Stack.Screen name="PasswordReset" component={PasswordResetScreen} />
+					<Stack.Screen name="Map" component={MapScreen} />
 				</>
 			)}
 		</Stack.Navigator>
