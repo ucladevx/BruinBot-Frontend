3ac650c9eee3973ba81acab7479263d85f35e755
Thilan Tran <44995807+thilan-tran@users.noreply.github.com>
2020-12-12 18:19:33 -0800

    Add navbar and hamburger (#22)
    * Add navbar and hamburger
    
    * Fix lint warnings, add logo to nav, update styles

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React, { useCallback, useContext, useEffect } from 'react';
 import 'react-native-gesture-handler';
 import * as Linking from 'expo-linking';
+
 import { Ctx, StateProvider } from './src/components/StateProvider';
 import LoginScreen from './src/containers/auth/LoginScreen';
 import PasswordResetScreen from './src/containers/auth/PasswordResetScreen';
@@ -11,6 +12,7 @@ import MapScreen from './src/containers/MapScreen';
 import InventoryModification from './src/containers/InventoryModification';
 import AddItem from './src/containers/AddItemScreen';
 import QrScreen from './src/containers/QrScreen';
+import NavMenuScreen from './src/containers/NavMenuScreen';
 
 export type RootStackParamList = {
 	Login: undefined;
@@ -75,26 +77,32 @@ const Home = () => {
 	});
 
 	return (
-		<Stack.Navigator headerMode="none">
-			{state.user ? (
-				<>
-					<Stack.Screen
-						name="InventoryModification"
-						component={InventoryModification}
-					/>
-					<Stack.Screen name="AddItem" component={AddItem} />
-					<Stack.Screen name="Qr" component={QrScreen} />
-					<Stack.Screen name="Map" component={MapScreen} />
-				</>
-			) : (
-				<>
-					<Stack.Screen name="Qr" component={QrScreen} />
-					<Stack.Screen name="Login" component={LoginScreen} />
-					<Stack.Screen name="Signup" component={SignupScreen} />
-					<Stack.Screen name="PasswordReset" component={PasswordResetScreen} />
-					<Stack.Screen name="Map" component={MapScreen} />
-				</>
-			)}
-		</Stack.Navigator>
+		<>
+			<NavMenuScreen />
+			<Stack.Navigator headerMode="none">
+				{state.user ? (
+					<>
+						<Stack.Screen
+							name="InventoryModification"
+							component={InventoryModification}
+						/>
+						<Stack.Screen name="AddItem" component={AddItem} />
+						<Stack.Screen name="Qr" component={QrScreen} />
+						<Stack.Screen name="Map" component={MapScreen} />
+					</>
+				) : (
+					<>
+						<Stack.Screen name="Qr" component={QrScreen} />
+						<Stack.Screen name="Login" component={LoginScreen} />
+						<Stack.Screen name="Signup" component={SignupScreen} />
+						<Stack.Screen
+							name="PasswordReset"
+							component={PasswordResetScreen}
+						/>
+						<Stack.Screen name="Map" component={MapScreen} />
+					</>
+				)}
+			</Stack.Navigator>
+		</>
 	);
 };
--- src/components/InventoryView.tsx
+++ src/components/InventoryView.tsx
@@ -188,4 +188,4 @@ const styles = StyleSheet.create({
 	},
 });
 
-export default React.memo(Inventory);
+export default Inventory;
