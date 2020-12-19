742161140bd6d256bdf1dc205de5f0ebfbd9f07e
Max Zhang <pipi-max@hotmail.com>
2020-12-19 02:19:47 -0800

    Link item creation to backend (#39)
    * Link item creation to backend
    
    * Fix typescript errors and add item to bots
    
    * Remove commented codes
    
    Co-authored-by: maxpluez <max@pop-os.localdomain>

---
+++ src/services/ItemService.ts
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+import Axios from 'axios';
+
+import { baseUrl } from '../config';
+import { Platform } from 'react-native';
+
+const axios = Axios.create({
+	baseURL: baseUrl,
+	withCredentials: true,
+});
+
+const addItem = async (
+	name: string,
+	price: number,
+	eventId: string,
+	uri: string,
+	botId: string,
+	quantity: number
+) => {
+	const img = {
+		uri: Platform.OS === 'android' ? uri : uri.replace('file://', ''),
+		type: 'image/jpeg',
+		name: uri.split('/').pop(),
+		// Need to hack the following fields to avoid typescript error
+		size: 0,
+		arrayBuffer: () =>
+			new Promise<ArrayBuffer>((resolve) => {
+				resolve(undefined);
+			}),
+		slice: () => new Blob(),
+		stream: () => new ReadableStream(),
+		text: () =>
+			new Promise<string>((resolve) => {
+				resolve('');
+			}),
+	};
+	const data = new FormData();
+	data.append('name', name);
+	data.append('price', `${price}`);
+	data.append('eventId', eventId);
+	data.append('img', img);
+	const res = await axios({
+		method: 'POST',
+		url: '/items/add',
+		data,
+		headers: { 'Content-Type': 'multipart/form-data' },
+	});
+	await axios.post('/bots/addItem', { botId, itemId: res.data._id, quantity });
+};
+
+export default { addItem };
