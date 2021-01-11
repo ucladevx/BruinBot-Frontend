f34fb9c75df6629cd4c71157ac870921a20244d9
Edward Zhang <edwardz123@gmail.com>
2021-01-11 13:19:37 -0800

    Fix reverted changes

--- src/services/ItemService.ts
+++ src/services/ItemService.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-import Axios from 'axios';
-import { Platform } from 'react-native';
 import { BASE_URL } from '../config';
+import { Platform } from 'react-native';
+import Axios from 'axios';
 
 const axios = Axios.create({
 	baseURL: BASE_URL,
