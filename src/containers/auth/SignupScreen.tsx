40920fb8c75547698e576851bc9306602733b56c
Edward Zhang <edwardz123@gmail.com>
2021-01-15 20:36:44 -0800

    Fix sign up and better error handling

--- src/containers/auth/SignupScreen.tsx
+++ src/containers/auth/SignupScreen.tsx
@@ -1,16 +1,16 @@
-import { Button, Input } from 'react-native-elements';
-import React, { useContext, useState } from 'react';
-
-import { FirebaseError } from 'firebase';
-import { StackNavigationProp } from '@react-navigation/stack';
-import Axios from 'axios';
-
+import { Alert } from 'react-native';
 import { BASE_URL } from '../../config';
+import { Button, Input } from 'react-native-elements';
 import { Ctx } from '../../components/StateProvider';
 import { PasswordInput, handleAuthErrors } from './FormUtils';
 import { RootStackParamList } from '../../../App';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { UserData } from '../../types/apiTypes';
 import { styles } from './FormStyles';
+import Axios from 'axios';
 import Form from './Form';
+import React, { useContext, useEffect, useState } from 'react';
+import firebase, { FirebaseError } from 'firebase';
 
 type Props = {
 	navigation: StackNavigationProp<RootStackParamList, 'Signup'>;
@@ -21,7 +21,12 @@ const SignupScreen = ({ navigation }: Props) => {
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
 	const [formErrors, setFormErrors] = useState({ email: '', password: '' });
-	const { state } = useContext(Ctx);
+	const [signingIn, setSigningIn] = useState(false);
+	const { state, dispatch } = useContext(Ctx);
+
+	useEffect(() => {
+		if (state.user != null) navigation.navigate('Qr');
+	}, [navigation, state.user]);
 
 	return (
 		<Form bigTitle title="Sign up" navigation={navigation} backButton>
@@ -42,6 +47,7 @@ const SignupScreen = ({ navigation }: Props) => {
 			<Button
 				title="Sign up"
 				buttonStyle={styles.formButton}
+				loading={signingIn}
 				onPress={() => {
 					if (password !== confirmPassword) {
 						setFormErrors({
@@ -50,25 +56,51 @@ const SignupScreen = ({ navigation }: Props) => {
 						});
 						return;
 					}
+					setSigningIn(true);
+					setFormErrors({ password: '', email: '' });
 					state.firebase
 						.auth()
 						.createUserWithEmailAndPassword(email, password)
-						.then(async (user) => {
-							const idToken = await user.getIdToken(true);
-							console.log(idToken);
-							//Works up to here. Just learn how to use axios
-							try {
-								const response = await Axios.post(BASE_URL + '/users/add', {
+						// Fetch Firebase JWT
+						.then((user) => Promise.all([user, user.getIdToken(true)]))
+						// Init user data in Mongo
+						.then(([user, token]) => {
+							return Promise.all([
+								user,
+								Axios.post(BASE_URL + 'users/add', {
 									username: email,
-									firebaseIdToken: idToken,
-								});
-								console.log(response.data);
-							} catch (error) {
-								console.error(error);
-							}
+									firebaseIdToken: token,
+								}),
+							]);
 						})
-						.catch((error: FirebaseError) => {
-							setFormErrors(handleAuthErrors(error));
+						// Populate user data in Context
+						.then(([user, res]) => {
+							const userData: UserData = res.data;
+							setSigningIn(false);
+							dispatch({
+								type: 'SET_USER',
+								user: {
+									_id: userData._id,
+									eventId: userData.eventId,
+									uid: user.uid,
+								},
+							});
+						})
+						.catch((error: FirebaseError | any) => {
+							setSigningIn(false);
+							// Only FirebaseError has code
+							if (error.code) setFormErrors(handleAuthErrors(error));
+							else {
+								console.log(error);
+								console.log('Error during signup! Deleting user...');
+								// We delete the user if we fail to add the user to mongodb
+								// This is to prevent the existence of firebase users with no corresponding mongo entry
+								firebase.auth().currentUser?.delete();
+								Alert.alert(
+									'Error connecting to server',
+									'Check your internet connection'
+								);
+							}
 						});
 				}}
 			/>
